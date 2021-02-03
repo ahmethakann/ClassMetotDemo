db9c23ad09ece94af85f68fc2ae032bed331e001
ahmethakann <66576626+ahmethakann@users.noreply.github.com>
2021-02-04 02:53:10 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("*******MÜŞTERİ EKLENİYOR********");
+            Console.WriteLine(musteri.Id + " " + musteri.AdSoyad + " " + musteri.Yas + " " + musteri.HesapBakiyesi+"TL");
+            Console.WriteLine("Müşteri Eklendi");
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("********mÜŞTERİ SİLİNİYOR********");
+            Console.WriteLine(musteri.Id + " " + musteri.AdSoyad + " " + musteri.Yas + " " + musteri.HesapBakiyesi+"TL");
+            Console.WriteLine("Müşteri Silindi");
+        }
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("*******Müşteriler Listeleniyor*******");
+            Console.WriteLine("Müşterinin Id'si: " + musteri.Id);
+            Console.WriteLine("Müşterinin Adı Soyadı: " + musteri.AdSoyad);
+            Console.WriteLine("Müşterinin Yaşı: " + musteri.Yas);
+            Console.WriteLine("Müşterinin Hesap Bakiyesi: " + musteri.HesapBakiyesi+"TL");
+            Console.WriteLine("----------");
+            
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.AdSoyad = "Mustafa Taş";
+            musteri1.Yas = 42;
+            musteri1.HesapBakiyesi = 356.48;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.AdSoyad = "Mehmet Demir";
+            musteri2.Yas = 28;
+            musteri2.HesapBakiyesi = 2456.78;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.AdSoyad = "Ayşe Gün";
+            musteri3.Yas = 33;
+            musteri3.HesapBakiyesi = 94.09;
+
+            
+            Musteri[] musteri = new Musteri[] {musteri1,musteri2,musteri3 };
+
+
+            Console.WriteLine("Hoşgeldiniz...");
+            Console.WriteLine("Müşteri eklemek için 1'e basın.");
+            Console.WriteLine("Müşteri silmek için 2'ye basın.");
+            Console.WriteLine("Müşterileri listelemek için 3'e basın.");
+           
+            int a = Console.Read();
+            //Console.WriteLine(a);
+
+            MusteriManager musteriManager = new MusteriManager();
+            switch (a)
+            {
+                case 49:
+                    musteriManager.MusteriEkle(musteri3);
+                    break;
+                case 50:
+                    musteriManager.MusteriSil(musteri1); 
+                    break;
+                case 51:
+                    foreach (Musteri musteriler in musteri)
+                    {
+                        musteriManager.MusteriListele(musteriler);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz işlem.");
+                    break;
+            }
+        }
+    }
+}
